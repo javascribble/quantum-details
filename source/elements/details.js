30374c64703ba20f178397e589495d3590ea00bb
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-16 13:27:00 -0400

    update templates

--- source/elements/details.js
+++ source/elements/details.js
@@ -1,11 +1,12 @@
-import { Component } from '../../references/quantum.js';
+import { Component, template } from '../../references/quantum.js';
+import html from '../templates/element.js';
 
 export class Details extends Component {
     constructor() {
         super();
     }
 
-    static template = document.querySelector('#quantum-details');
+    static template = template(html);
 
     static attributes = [];
 }
