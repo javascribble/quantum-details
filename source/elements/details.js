31dc331cee0d5d1dd1348defaa6a8687f7b8c346
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:39:13 -0400

    update build

--- source/elements/details.js
+++ source/elements/details.js
@@ -1,5 +1,9 @@
+import details from '../templates/details.js';
+
 export class Details extends Quantum {
     constructor() {
         super();
     }
 }
+
+Details.define('quantum-details', details);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Details } from './elements/details.js';
-import details from './templates/details.js';
-
-Details.define('quantum-details', details);
