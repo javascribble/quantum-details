6aea401ccf4dc42a4580dd09f5b8c829419e8fbf
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-10-14 08:19:27 -0400

    add default style

--- source/templates/details.js
+++ source/templates/details.js
@@ -1,5 +1,24 @@
 export default `
 <style>
+    summary {
+        cursor: pointer;
+        transition: color 0s;
+        letter-spacing: var(--letter-spacing);
+        text-transform: uppercase;
+        font-weight: bold;
+        outline: none;
+    }
 
+        summary:hover {
+            color: var(--primary-hover-color);
+        }
+
+        summary:active {
+            color: var(--primary-active-color);
+        }
+
+    details {
+        
+    }
 </style>
 `;
